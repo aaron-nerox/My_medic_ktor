7e87349de4a052b4fa71baeea7c0ce52b1e51adc
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-05-30 19:29:18 +0100

    start: backend project for medic mobile

---
+++ main/kotlin/tech/nerostarx/Application.kt
@@ -0,0 +1,19 @@
+package tech.nerostarx
+
+import io.ktor.application.*
+import tech.nerostarx.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    configureRouting()
+    configureSecurity()
+    configureSerialization()
+}
