02988daae3a640399c8d77c5ebc98ea7514dd5ca
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-06-08 00:55:57 +0100

    feat: Auth returning the user object directly without specifying any new attribute

--- main/kotlin/tech/nerostarx/plugins/Security.kt
+++ main/kotlin/tech/nerostarx/plugins/Security.kt
@@ -2,6 +2,7 @@ package tech.nerostarx.plugins
 
 import io.ktor.auth.*
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.jetbrains.exposed.sql.ResultRow
@@ -11,6 +12,10 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import tech.nerostarx.databases.PGDataBase
 import tech.nerostarx.models.AuthUser
 import tech.nerostarx.models.Authusr
+import tech.nerostarx.models.Doctors
+import tech.nerostarx.models.Patients
+import tech.nerostarx.routes.toDoctor
+import tech.nerostarx.routes.toPatient
 
 fun Application.configureSecurity() {
     
@@ -47,7 +52,27 @@ fun Application.configureSecurity() {
             get("/auth/login") {
                 val userPrincipal = call.principal<UserIdPrincipal>()
 				if(userPrincipal != null){
-					call.respond(userPrincipal.name)
+					val authUser = transaction(PGDataBase.dataBaseInstance){
+						AuthUser.select{
+							AuthUser.userPhone eq userPrincipal.name
+						}.map { toAuthUser(it) }
+					}[0]
+
+					if(authUser.userType == "patient"){
+						val user = transaction(PGDataBase.dataBaseInstance){
+							Patients.select {
+								Patients.idPatient eq authUser.idUser
+							}.map { toPatient(it) }
+						}[0]
+						call.respond(user)
+					}else{
+						val user = transaction(PGDataBase.dataBaseInstance){
+							Doctors.select {
+								Doctors.idDoc eq authUser.idUser
+							}.map { toDoctor(it) }
+						}[0]
+						call.respond(user)
+					}
 				}else{
 					call.respondText("Error: Password or phone number error")
 				}
