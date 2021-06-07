e9a7cb3af86164f9596c715b1c34ec609d77404b
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-06-07 23:06:08 +0100

    add: the hikari pooling and the database credentials

--- main/kotlin/tech/nerostarx/databases/PGDataBase.kt
+++ main/kotlin/tech/nerostarx/databases/PGDataBase.kt
@@ -1,13 +1,22 @@
 package tech.nerostarx.databases
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.jetbrains.exposed.sql.Database
 
 object PGDataBase {
 
+    private val dataSourceConfig = HikariConfig().apply {
+        jdbcUrl         = "jdbc:postgresql://localhost:5432/my_medic_db"
+        driverClassName = "org.postgresql.Driver"
+        username        = "nerostarx"
+        password        = "TheNewWorld"
+        maximumPoolSize = 10
+    }
+
+    private val datasource = HikariDataSource(dataSourceConfig)
+
     val dataBaseInstance by lazy{
-        Database.connect("jdbc:postgresql://localhost:5432/kotlin_db"
-            , "org.postgresql.Driver"
-            , "nerostarx"
-            , "TheNewWorld")
+        Database.connect(datasource)
     }
 }
