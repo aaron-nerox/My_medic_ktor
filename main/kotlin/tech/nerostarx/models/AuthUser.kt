bf5ab12ecb661005b0cc4b642f53f9205a40eb29
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-06-08 00:35:16 +0100

    feat: Auth user with phone number

--- main/kotlin/tech/nerostarx/models/AuthUser.kt
+++ main/kotlin/tech/nerostarx/models/AuthUser.kt
@@ -1,10 +1,20 @@
 package tech.nerostarx.models
 
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.Table
 
 
-object AuthUser: Table() {
+data class Authusr(
+    val idAuth: Int,
+    val userPhone: String,
+    val userPassword: String,
+    val userType: String,
+    val idUser:Int
+)
+
+
+object AuthUser: Table("AuthUser") {
     val idAuth: Column<Int> = integer("id_auth").autoIncrement()
     val userPhone: Column<String> = text("user_phone")
     val userPassword: Column<String> = text("user_password")
--- main/kotlin/tech/nerostarx/plugins/Security.kt
+++ main/kotlin/tech/nerostarx/plugins/Security.kt
@@ -4,22 +4,40 @@ import io.ktor.auth.*
 import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.and
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import tech.nerostarx.databases.PGDataBase
+import tech.nerostarx.models.AuthUser
+import tech.nerostarx.models.Authusr
 
 fun Application.configureSecurity() {
     
     authentication {
 
     	    form(name = "auth") {
-				//TODO: search the user from the database
-
     	        userParamName = "user"
     	        passwordParamName = "password"
     	        validate { credentials ->
-					if(credentials.name == "aaron" && credentials.password == "pass"){
-						UserIdPrincipal(credentials.name)
-					}else{
-						null
+					val user = transaction(PGDataBase.dataBaseInstance){
+						AuthUser.select {
+							AuthUser.userPhone eq credentials.name and(AuthUser.userPassword eq credentials.password)
+						}.map{ toAuthUser(it) }
 					}
+
+					if (user.isEmpty()){
+						return@validate null
+					}else{
+						val authUser = user[0]
+						return@validate if(credentials.name == authUser.userPhone
+							&& credentials.password == authUser.userPassword){
+							UserIdPrincipal(authUser.userPhone)
+						}else{
+							null
+						}
+					}
+
 				}
     	    }
     	}
@@ -28,14 +46,23 @@ fun Application.configureSecurity() {
         authenticate("auth") {
             get("/auth/login") {
                 val userPrincipal = call.principal<UserIdPrincipal>()
-
 				if(userPrincipal != null){
-					call.respondText("Hello ${userPrincipal.name}")
+					call.respond(userPrincipal.name)
 				}else{
-					call.respondText("Password or username error")
+					call.respondText("Error: Password or phone number error")
 				}
 
             }
         }
     }
+}
+
+fun toAuthUser(row: ResultRow): Authusr {
+	return Authusr(
+		row[AuthUser.idAuth],
+		row[AuthUser.userPhone],
+		row[AuthUser.userPassword],
+		row[AuthUser.userType],
+		row[AuthUser.idUser]
+	)
 }
