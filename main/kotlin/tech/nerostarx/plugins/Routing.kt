9a4551c22a19fddc8ba47a1f1bc920d40eb421bc
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-06-07 22:27:58 +0100

    fix: routing and security

--- main/kotlin/tech/nerostarx/plugins/Routing.kt
+++ main/kotlin/tech/nerostarx/plugins/Routing.kt
@@ -1,19 +1,13 @@
 package tech.nerostarx.plugins
 
 import io.ktor.routing.*
-import io.ktor.http.*
 import io.ktor.application.*
-import io.ktor.auth.*
-import io.ktor.response.*
-import io.ktor.request.*
-import tech.nerostarx.routes.configureAdviceRoutes
 import tech.nerostarx.routes.configureAppointmentRoutes
 import tech.nerostarx.routes.configureDoctorRoutes
 import tech.nerostarx.routes.configureTreatmentRoutes
 
 fun Application.configureRouting() {
     routing {
-        configureAdviceRoutes()
         configureAppointmentRoutes()
         configureDoctorRoutes()
         configureTreatmentRoutes()
--- main/kotlin/tech/nerostarx/plugins/Security.kt
+++ main/kotlin/tech/nerostarx/plugins/Security.kt
@@ -1,10 +1,8 @@
 package tech.nerostarx.plugins
 
 import io.ktor.auth.*
-import io.ktor.util.*
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 
 fun Application.configureSecurity() {
