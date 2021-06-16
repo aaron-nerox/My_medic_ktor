0537fd5b3f2c0cf310370be6807342e2e10bd03d
Nerostarx <35573892+NeroStarx@users.noreply.github.com>
2021-06-16 11:26:41 +0100

    feat: add search by date and doc fix: adding the appointment generates the date directly

--- main/kotlin/tech/nerostarx/models/Appointment.kt
+++ main/kotlin/tech/nerostarx/models/Appointment.kt
@@ -4,13 +4,19 @@ import kotlinx.serialization.Serializable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.Table
 
+@Serializable
+data class AptParams(
+    val idDoc: Int?,
+    val date: String?,
+)
 
 @Serializable
 data class Appointment(
     var idApt: Int? = null,
     val idDoc: Int,
     val idPatient: Int,
-    val date: String,
+    var date: String? = null,
+    var hour: String? = null
 )
 
 object Appointments: Table() {
@@ -18,6 +24,7 @@ object Appointments: Table() {
     val idDoc: Column<Int> = integer("id_doctor")
     val idPatient: Column<Int> = integer("id_patient")
     val date: Column<String> = text("apt_date")
+    val hour: Column<String> = text("apt_hour")
 
     override val primaryKey = PrimaryKey(idApt)
 }
--- main/kotlin/tech/nerostarx/routes/AppointmentRoutes.kt
+++ main/kotlin/tech/nerostarx/routes/AppointmentRoutes.kt
@@ -6,41 +6,58 @@ import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import org.jetbrains.exposed.sql.ResultRow
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
 import tech.nerostarx.databases.PGDataBase
 import tech.nerostarx.models.Appointment
 import tech.nerostarx.models.Appointments
+import tech.nerostarx.models.AptParams
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 fun Route.configureAppointmentRoutes(){
     route("/appointment"){
-        get("{idDoc}"){
-            val id = call.parameters["idDoc"] ?: return@get call
+        post{
+            val params = call.receive<AptParams>()
+            val id = params.idDoc ?: return@post call
                 .respondText("Invalid id", status = HttpStatusCode.BadRequest)
+            val date = params.date ?: return@post call
+                .respondText("Invalid date", status = HttpStatusCode.BadRequest)
 
             val appointments = transaction(PGDataBase.dataBaseInstance){
                 Appointments.select {
-                    Appointments.idApt eq id.toInt()
+                    Appointments.idDoc eq id and(Appointments.date eq date)
                 }.map { toAppointment(it) }
             }
 
             call.respond(status = HttpStatusCode.OK, appointments)
         }
 
-        post{
+        post("/add"){
             val pendingAppointment = call.receive<Appointment>()
+
+            val formatPattern = DateTimeFormatter.ofPattern("dd-MMMM-yyyy")
+            val hourFormatPattern = DateTimeFormatter.ofPattern("HH:MM")
+            val calendar = LocalDateTime.now()
+            val currentDate = calendar.format(formatPattern)
+
+            val currentTime = calendar.format(hourFormatPattern)
 
             val result = transaction(PGDataBase.dataBaseInstance){
                 Appointments.insert {
                     it[idDoc] = pendingAppointment.idDoc
                     it[idPatient] = pendingAppointment.idPatient
-                    it[date] = pendingAppointment.date
+                    it[date] = currentDate
+                    it[hour] = currentTime
                 }.resultedValues
             }
 
             if(result != null){
-                call.respond(HttpStatusCode.Created, "sucess created")
+                call.respond(HttpStatusCode.Created, result.map { toAppointment(it) })
             }else{
                 call.respond(HttpStatusCode.InternalServerError, "failed not created")
             }
@@ -53,6 +70,7 @@ fun toAppointment(row: ResultRow):Appointment{
         row[Appointments.idApt],
         row[Appointments.idDoc],
         row[Appointments.idPatient],
-        row[Appointments.date]
+        row[Appointments.date],
+        row[Appointments.hour]
     )
 }
